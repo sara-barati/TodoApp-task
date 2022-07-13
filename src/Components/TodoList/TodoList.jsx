eeb0bf567d64c25c68d3f2304c9f12e9cfe1df5d
sara <sarabarati045@gmail.com>
2022-07-13 18:08:17 +0430

    feat:install iconyfi

--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import "./TodoList.scss";
+import { Icon } from '@iconify/react';
 import { ListGroup, Row, Col } from "react-bootstrap";
 export default function TodoList({ remove, title, handleChange, done }) {
   return (
     <ListGroup.Item>
       <Row>
         <Col xs={1}>
-          <input type="checkbox" onChange={handleChange} />
+        <span id="checkbox">
+  <input id="checkbox-element" type="checkbox" onChange={handleChange}/>  
+   {/* <span className="icon-check" /> */}
+
+   <Icon className="icon-check" icon="mdi:check" />
+</span>
+          {/* <input type="checkbox" onChange={handleChange}> <span className="icon-check"/></input> */}
+  
         </Col>
         <Col xs={10} className={done ? "doneTrue" : "false"}>
           {title}
         </Col>
         <Col xs={1}>
           <button onClick={remove}>
-            <span className="icon-trash"> </span>
+            <span  className="icon-trash"> </span>
           </button>
         </Col>
       </Row>
